d981365373f6594023c0fea1fceefa7f66e8b02b
JRWOOS02 <josh.woosley@gmail.com>
2017-03-27 12:00:05 -0400

    Project 2

---
+++ Program 2/Program 2/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Program_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FreshmanRadio = new System.Windows.Forms.RadioButton();
+            this.SophomoreRadio = new System.Windows.Forms.RadioButton();
+            this.JuniorRadio = new System.Windows.Forms.RadioButton();
+            this.SeniorRadio = new System.Windows.Forms.RadioButton();
+            this.GradeLabel = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.NameText = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.Output = new System.Windows.Forms.Label();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // FreshmanRadio
+            // 
+            this.FreshmanRadio.AutoSize = true;
+            this.FreshmanRadio.Location = new System.Drawing.Point(268, 133);
+            this.FreshmanRadio.Name = "FreshmanRadio";
+            this.FreshmanRadio.Size = new System.Drawing.Size(106, 24);
+            this.FreshmanRadio.TabIndex = 0;
+            this.FreshmanRadio.TabStop = true;
+            this.FreshmanRadio.Text = "Freshman";
+            this.FreshmanRadio.UseVisualStyleBackColor = true;
+            // 
+            // SophomoreRadio
+            // 
+            this.SophomoreRadio.AutoSize = true;
+            this.SophomoreRadio.Location = new System.Drawing.Point(268, 190);
+            this.SophomoreRadio.Name = "SophomoreRadio";
+            this.SophomoreRadio.Size = new System.Drawing.Size(117, 24);
+            this.SophomoreRadio.TabIndex = 1;
+            this.SophomoreRadio.TabStop = true;
+            this.SophomoreRadio.Text = "Sophomore";
+            this.SophomoreRadio.UseVisualStyleBackColor = true;
+            // 
+            // JuniorRadio
+            // 
+            this.JuniorRadio.AutoSize = true;
+            this.JuniorRadio.Location = new System.Drawing.Point(268, 242);
+            this.JuniorRadio.Name = "JuniorRadio";
+            this.JuniorRadio.Size = new System.Drawing.Size(77, 24);
+            this.JuniorRadio.TabIndex = 2;
+            this.JuniorRadio.TabStop = true;
+            this.JuniorRadio.Text = "Junior";
+            this.JuniorRadio.UseVisualStyleBackColor = true;
+            // 
+            // SeniorRadio
+            // 
+            this.SeniorRadio.AutoSize = true;
+            this.SeniorRadio.Location = new System.Drawing.Point(268, 298);
+            this.SeniorRadio.Name = "SeniorRadio";
+            this.SeniorRadio.Size = new System.Drawing.Size(80, 24);
+            this.SeniorRadio.TabIndex = 3;
+            this.SeniorRadio.TabStop = true;
+            this.SeniorRadio.Text = "Senior";
+            this.SeniorRadio.UseVisualStyleBackColor = true;
+            // 
+            // GradeLabel
+            // 
+            this.GradeLabel.AutoSize = true;
+            this.GradeLabel.Location = new System.Drawing.Point(264, 91);
+            this.GradeLabel.Name = "GradeLabel";
+            this.GradeLabel.Size = new System.Drawing.Size(204, 20);
+            this.GradeLabel.TabIndex = 4;
+            this.GradeLabel.Text = "What is your current grade?";
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.Location = new System.Drawing.Point(60, 91);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(184, 47);
+            this.NameLabel.TabIndex = 5;
+            this.NameLabel.Text = "What is the first letter of your last name?";
+            // 
+            // NameText
+            // 
+            this.NameText.Location = new System.Drawing.Point(64, 187);
+            this.NameText.Name = "NameText";
+            this.NameText.Size = new System.Drawing.Size(145, 26);
+            this.NameText.TabIndex = 6;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(490, 290);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(94, 32);
+            this.CalculateButton.TabIndex = 7;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(486, 133);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(281, 80);
+            this.Output.TabIndex = 8;
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.Location = new System.Drawing.Point(486, 91);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(147, 20);
+            this.OutputLabel.TabIndex = 9;
+            this.OutputLabel.Text = "You can register on";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 449);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.NameText);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.GradeLabel);
+            this.Controls.Add(this.SeniorRadio);
+            this.Controls.Add(this.JuniorRadio);
+            this.Controls.Add(this.SophomoreRadio);
+            this.Controls.Add(this.FreshmanRadio);
+            this.Name = "Form1";
+            this.Text = "Program 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton FreshmanRadio;
+        private System.Windows.Forms.RadioButton SophomoreRadio;
+        private System.Windows.Forms.RadioButton JuniorRadio;
+        private System.Windows.Forms.RadioButton SeniorRadio;
+        private System.Windows.Forms.Label GradeLabel;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox NameText;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Label Output;
+        private System.Windows.Forms.Label OutputLabel;
+    }
+}
+
---
+++ Program 2/Program 2/Form1.cs
@@ -0,0 +1,151 @@
+﻿//Joshua Woosley
+//Program 2
+//Due 10/27/14
+//CIS 199-01
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            //Defines the char's for all letters and one for the entered string on the program
+            char charvariable;
+            char L1, L2, L3, L4, L5, L6, L7, L8, L9, L10,
+            L11, L12, L13, L14, L15, L16, L17, L18,
+            L19, L20, L21, L22, L23, L24, L25, L26;
+            L1 = 'A';
+            L2 = 'B';
+            L3 = 'C';
+            L4 = 'D';
+            L5 = 'E';
+            L6 = 'F';
+            L7 = 'G';
+            L8 = 'H';
+            L9 = 'I';
+            L10 = 'J';
+            L11 = 'K';
+            L12 = 'L';
+            L13 = 'M';
+            L14 = 'N';
+            L15 = 'O';
+            L16 = 'P';
+            L17 = 'Q';
+            L18 = 'R';
+            L19 = 'S';
+            L20 = 'T';
+            L21 = 'U';
+            L22 = 'V';
+            L23 = 'W';
+            L24 = 'X';
+            L25 = 'Y';
+            L26 = 'Z';
+
+            //If statement to test for just one letter entered and if not will throw an error.
+            if (char.TryParse(NameText.Text, out charvariable)) { }
+            else
+            {
+                MessageBox.Show("Only the first letter of your last name is required");
+            }
+
+            //set the output to clear each time the button is hit
+            Output.Text=("");
+
+            //Checks if freshman radio is checked and then tests for what letter was entered and gives the date for registering
+            if (FreshmanRadio.Checked)
+            {
+                if (charvariable == L1 || charvariable == L2 || charvariable == char.ToLower(L1) || charvariable == char.ToLower(L2))
+                    Output.Text = ("Thursday, November 13th at 2pm");
+                if (charvariable == L3 || charvariable == L4 || charvariable == char.ToLower(L3) || charvariable == char.ToLower(L4))
+                    Output.Text = ("Thursday, November 13th at 4pm");
+                if (charvariable == L5 || charvariable == L6 || charvariable == char.ToLower(L5) || charvariable == char.ToLower(L6))
+                    Output.Text = ("Friday, November 14th at 8:30am");
+                if (charvariable >= L7 && charvariable <= L9 || charvariable >= char.ToLower(L7) && charvariable <= char.ToLower(L9))
+                    Output.Text = ("Friday, November 14th at 10:00am");
+                if (charvariable >= L10 && charvariable <= L12 || charvariable >= char.ToLower(L10) && charvariable <= char.ToLower(L12))
+                    Output.Text = ("Friday, November 14th at 11:30am");
+                if (charvariable >= L13 && charvariable <= L15 || charvariable >= char.ToLower(L13) && charvariable <= char.ToLower(L15))
+                    Output.Text = ("Friday, November 14th at 2pm");
+                if (charvariable >= L16 && charvariable <= L18 || charvariable >= char.ToLower(L16) && charvariable <= char.ToLower(L18))
+                    Output.Text = ("Friday, November 14th at 4pm");
+                if (charvariable == L19 || charvariable == L20 || charvariable == char.ToLower(L19) || charvariable == char.ToLower(L20))
+                    Output.Text = ("Thursday, November 13th at 8:30am");
+                if (charvariable == L21 || charvariable == L22|| charvariable == char.ToLower(L21) || charvariable == char.ToLower(L22))
+                    Output.Text = ("Thursday, November 13th at 10:00am");
+                if (charvariable >= L23 && charvariable <= L26|| charvariable >= char.ToLower(L23) && charvariable <= char.ToLower(L26))
+                    Output.Text = ("Thursday, November 13th at 11:30am");
+            }
+
+            //Checks if sophomore radio is checked and then tests for what letter was entered and gives the date for registering
+            if (SophomoreRadio.Checked)
+            {
+                if (charvariable == L1 || charvariable == L2 || charvariable == char.ToLower(L1) || charvariable == char.ToLower(L2)) 
+                    Output.Text = ("Tuesday, November 11th at 2pm");
+                if (charvariable == L3 || charvariable == L4 || charvariable == char.ToLower(L3) || charvariable == char.ToLower(L4))
+                    Output.Text = ("Tuesday, November 11th at 4pm");
+                if (charvariable == L5 || charvariable == L6 || charvariable == char.ToLower(L5) || charvariable == char.ToLower(L6))
+                    Output.Text = ("Wednesday, November 12th at 8:30am");
+                if (charvariable >= L7 && charvariable <= L9 || charvariable >= char.ToLower(L7) && charvariable <= char.ToLower(L9))
+                    Output.Text = ("Wednesday, November 12th at 10:00am");
+                if (charvariable >= L10 && charvariable <= L12 || charvariable >= char.ToLower(L10) && charvariable <= char.ToLower(L12))
+                    Output.Text = ("Wednesday, November 12th at 11:30am");
+                if (charvariable >= L13 && charvariable <= L15 || charvariable >= char.ToLower(L13) && charvariable <= char.ToLower(L15))
+                    Output.Text = ("Wednesday, November 12th at 2pm");
+                if (charvariable >= L16 && charvariable <= L18 || charvariable >= char.ToLower(L16) && charvariable <= char.ToLower(L18))
+                    Output.Text = ("Wednesday, November 12th at 4pm");
+                if (charvariable == L19 || charvariable == L20 || charvariable == char.ToLower(L19) || charvariable == char.ToLower(L20))
+                    Output.Text = ("Tuesday, November 11th at 8:30am");
+                if (charvariable == L21 || charvariable == L22 || charvariable == char.ToLower(L21) || charvariable == char.ToLower(L22))
+                    Output.Text = ("Tuesday, November 11th at 10:00am");
+                if (charvariable >= L23 && charvariable <= L26 || charvariable >= char.ToLower(L23) && charvariable <= char.ToLower(L26))
+                    Output.Text = ("Tuesday, November 11th at 11:30am");
+            }
+
+            //Checks if junior radio is checked and then tests for what letter was entered and gives the date for registering
+            if (JuniorRadio.Checked)
+            {
+                if (charvariable >= L1 && charvariable <= L4 || charvariable >= char.ToLower(L1) && charvariable <= char.ToLower(L4))
+                    Output.Text = ("Monday, November 10th at 10:00am");
+                if (charvariable >= L5 && charvariable <= L9 || charvariable >= char.ToLower(L5) && charvariable <= char.ToLower(L9))
+                    Output.Text = ("Monday, November 10th at 11:30am");
+                if (charvariable >= L10 && charvariable <= L15 || charvariable >= char.ToLower(L10) && charvariable <= char.ToLower(L15))
+                    Output.Text = ("Monday, November 10th at 2:00pm");
+                if (charvariable >= L16 && charvariable <= L18 || charvariable >= char.ToLower(L16) && charvariable <= char.ToLower(L18))
+                    Output.Text = ("Monday, November 10th at 4:00pm");
+                if (charvariable >= L19 && charvariable <= L26 || charvariable >= char.ToLower(L19) && charvariable <= char.ToLower(L26))
+                    Output.Text = ("Monday, November 10th at 8:30am");
+            }
+
+            //Checks if senior radio is checked and then tests for what letter was entered and gives the date for registering
+            if (SeniorRadio.Checked)
+            {
+                if (charvariable >= L1 && charvariable <= L4 || charvariable >= char.ToLower(L1) && charvariable <= char.ToLower(L4))
+                    Output.Text = ("Friday, November 7th at 10:00am");
+                if (charvariable >= L5 && charvariable <= L9 || charvariable >= char.ToLower(L5) && charvariable <= char.ToLower(L9))
+                    Output.Text = ("Friday, November 7th at 11:30am");
+                if (charvariable >= L10 && charvariable <= L15 || charvariable >= char.ToLower(L10) && charvariable <= char.ToLower(L15))
+                    Output.Text = ("Friday, November 7th at 2:00pm");
+                if (charvariable >= L16 && charvariable <= L18 || charvariable >= char.ToLower(L16) && charvariable <= char.ToLower(L18))
+                    Output.Text = ("Friday, November 7th at 4:00pm");
+                if (charvariable >= L19 && charvariable <= L26 || charvariable >= char.ToLower(L19) && charvariable <= char.ToLower(L26))
+                    Output.Text = ("Friday, November 7th at 8:30am");
+            }
+            
+        }
+    }
+}
